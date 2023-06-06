3bbc76c3db1e9ba51cfc4b69af0ac93bbd4d8706
Igor Haas <igordhaas@gmail.com>
2023-06-05 21:44:34 -0300

    versão @alpha0.0.1

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 let canvas= document.getElementById("canvas");
 let ctx= canvas.getContext('2d');
 let player = new Player(150,15,ctx)
+let ball = new Ball(470,310,ctx);
 
 canvas.height = 620;
 canvas.width = 940;
 
 
-player.draw();
-player.updatePosition();
+function drawMargins(){
+    ctx.fillStyle = '#000'
+    ctx.fillRect(8,8,922,602);
+    ctx.clearRect(10,10,918,598)
+}
+function drawLimit(){
+    ctx.fillStyle = '#990000'
+    ctx.fillRect(10,608,918,2);
+}
+
+ball.player = player;
 
 function update(){
-    // canvas.width = 940;
+    if(ctx.pause == true) return;
+    canvas.width = 940;
+    drawMargins();
+    drawLimit();
+    ball.update();
+    ball.draw();
+    player.draw();
+    player.update();
     requestAnimationFrame(update);
 }
 
